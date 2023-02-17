25864fa642b3a1fe60234b3e34dfcc4d4ae990ca
adevmir <ademirvalonqar@gmail.com>
2023-02-16 21:45:29 -0300

    [Requisito - 8] - Recebe um dicionario job com max_salary e min_salary e um valor salario e depois de conferir os dados recebidos retorna se o salario se encontra entre o max e min

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -28,28 +28,25 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
+    print(job, salary)
+    if not "max_salary" in job \
+            or not "min_salary" in job:
+        raise ValueError
+    max_salary = job["max_salary"]
+    min_salary = job["min_salary"]
+    if type(max_salary) == str:
+        max_salary = int(max_salary)
+    if type(min_salary) == str:
+        min_salary = int(min_salary)
+    if type(salary) == str:
+        salary = int(salary)
+    if type(max_salary) != int \
+            or type(min_salary) != int \
+            or type(salary) != int:
+        raise ValueError
+    if min_salary > max_salary:
+        raise ValueError
+    return max_salary >= salary >= min_salary
     raise NotImplementedError
 
 
