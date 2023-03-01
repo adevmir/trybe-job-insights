e66309efb3dabca457b3cb670dd1751854329b96
adevmir <ademirvalonqar@gmail.com>
2023-03-01 18:13:20 -0300

    req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+
+
+SALARY_COUNTER = 598
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "salary") == SALARY_COUNTER
